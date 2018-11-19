cda66c9ed785a7fcf46cb8e4a5eaa00d6288bb96
bernard <bernardkissi18@gmail.com>
2018-11-19 06:29:53 -0800

    Updated project root files

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,11 +13,6 @@
               <p>Content of no border type. Content of no border type. Content of no border type. Content of no border type. </p>
           </Card>
       </div>
-      <el-steps :active="1">
-          <el-step title="Step 1" description="Some description"></el-step>
-          <el-step title="Step 2" description="Some description"></el-step>
-          <el-step title="Step 3" description="Some description"></el-step>
-      </el-steps>
 
   </div>
 </template>
